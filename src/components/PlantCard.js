b05e596a1f30a838938c5c6c31db4e909d4dbb4e
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-10 12:48:07 +0300

    Plant details

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,16 @@
+
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {plant.isSoldOut ? (
         <button>Out of Stock</button>
+      ) : (
+        <button className="primary">In Stock</button>
       )}
     </li>
   );
