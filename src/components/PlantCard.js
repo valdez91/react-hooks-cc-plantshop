3cdcd5a4654e33737af76017443be273d0423b74
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-10 12:52:17 +0300

    all about plantdetails

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,21 @@
-
+// Import React from the react library
 import React from "react";
 
+// Define a function component called PlantCard
 function PlantCard({ plant }) {
+  // The component expects a prop called 'plant' which is an object containing plant data
   return (
+    // Return a list item element with a class of 'card' and a data-testid of 'plant-item'
     <li className="card" data-testid="plant-item">
+       {/* Display the plant's image using an img element */}
       <img src={plant.image} alt={plant.name} />
+       {/* Display the plant's name using an h4 element */}
       <h4>{plant.name}</h4>
+       {/* Display the plant's price using a p element */}
       <p>Price: {plant.price}</p>
-      {plant.isSoldOut ? (
+      {/*  If the plant is sold out, display a button with the text 'Out of Stock' */}
+      {/*  Otherwise, display a button with the text 'In Stock' and a class of 'primary' */}
+      {plant.isSoldOut? (
         <button>Out of Stock</button>
       ) : (
         <button className="primary">In Stock</button>
@@ -16,4 +24,5 @@ function PlantCard({ plant }) {
   );
 }
 
+// Export the PlantCard component as the default export
 export default PlantCard;
