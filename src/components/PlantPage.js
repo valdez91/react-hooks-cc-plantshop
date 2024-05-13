28f4be33387b0f3873a000609e369f19c7641543
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-13 07:48:40 +0300

    fetching data

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
-import plantsData from "./db.json";
 
 function PlantPage() {
-  const [plants, setPlants] = useState(plantsData.plants);
-  const [filteredPlants, setFilteredPlants] = useState(plantsData.plants);
+  const [plants, setPlants] = useState([]);
+  const [filteredPlants, setFilteredPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => {
+        setPlants(data); // Set all plants
+        setFilteredPlants(data); // Initially, set filtered plants to all plants
+      })
+      .catch((error) => {
+        console.error("Error fetching plants:", error);
+      });
+  }, []);
 
   const handleSearch = (query) => {
     const filtered = plants.filter((plant) =>
